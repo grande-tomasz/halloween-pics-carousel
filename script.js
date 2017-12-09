feb32d23089b94025d4ee8d1ed76cdab127affd7
Tomasz Grande <grande.tomasz@gmail.com>
2017-12-10 00:58:41 +0100

    Add side navigation links functionality

--- script.js
+++ script.js
@@ -1,27 +1,62 @@
 $(function() {
   var carouselList = $("#picsCarousel ul");
-  var intervalID = setInterval(changeSlide, 4000);
+  var slideWidth = $('#picsCarousel img').width();
+  var slideChangeDuration = 1000;
+  var picShowingDuration = 3000;
+
+  var firstItem = carouselList.find("li:first");
+  var lastItem = carouselList.find("li:last");
+  lastItem.prependTo(carouselList);
+  var intervalID = setInterval(function() {changeSlide("next")}, slideChangeDuration+picShowingDuration);
   // console.log("interval set");
 
-  function changeSlide() {
+  $('#previousSlide').click(function() {
+    // console.log("previousSlide function started");
+    restartInterval();
+    changeSlide("previous");
+  });
+
+  $('#nextSlide').click(function() {
+    // console.log("nextSlide function started");
+    restartInterval();
+    changeSlide("next");
+  });
+
+  function restartInterval() {
+    clearInterval(intervalID);
+    intervalID = setInterval(function() {changeSlide("next")}, slideChangeDuration+picShowingDuration);
+  }
+
+  function changeSlide(direction) {
     // console.log("changeSlide function started");
+    var margin = -2*slideWidth;
+    if (direction === "previous") {
+      margin = 0
+    }
+
     carouselList.animate(
       {
-        "marginLeft": "-800"
+        marginLeft: margin
       }, 
-      1000, 
-      moveFirstSlide
+      slideChangeDuration, 
+      function() {moveFirstSlide(direction)}      
     );
   }
 
-  function moveFirstSlide() {
+  function moveFirstSlide(direction) {
     // console.log("moveFirstSlide function started");
-    var firstItem = carouselList.find("li:first");
-    var lastItem = carouselList.find("li:last");
-    lastItem.after(firstItem);  
+    // lastItem.after(firstItem);
+    if (direction === "previous") {
+      lastItem = carouselList.find("li:last");
+      lastItem.prependTo(carouselList);
+    }
+    else {
+      firstItem = carouselList.find("li:first");
+      firstItem.appendTo(carouselList);      
+    }
     carouselList.css(
       {
-        "marginLeft": "0"
+        marginLeft: -slideWidth
       }
     );
   }
